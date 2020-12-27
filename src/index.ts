108892e3ac32c1bdc368522c7ac580a85b37d085
Josh Prewer <josh.prewer@focusrite.com>
2020-12-27 17:17:39 +0000

    Better log messaging

--- src/index.ts
+++ src/index.ts
@@ -17,25 +17,30 @@ async function main () {
 
   while (true) {
     try {
-      const checkProductResponses = await Promise.all([
+      const responses = await Promise.all([
         argosHandler(),
         gameHandler(),
         // veryHandler(),
         amazonHandler()
       ])
 
-      const availableResponse = checkProductResponses.find(
-        (response) => response.isAvailable
-      )
+      const unavailableSites = responses
+        .filter(element => !element.isAvailable)
+        .map(element => element.name)
 
-      if (availableResponse) {
-        const message = `PS5 might be available at ${availableResponse.name}`
-        console.log(message)
+      const availableSites = responses
+        .filter(element => element.isAvailable)
+        .map(element => element.name)
+
+      if (availableSites.length !== 0) {
+        const message = `PS5 might be available at ${availableSites.join(', ')}`
+        console.log('\x1b[32m%s\x1b[0m', message)
         sendSms(message)
         break
       }
 
-      console.log('PS5 is unavailable')
+      unavailableSites.forEach(site => console.log('\x1b[31m%s\x1b[0m', `PS5 is unavailable at ${site}`))
+
       console.log('------------- SLEEPING -------------')
       await sleep(TIMEOUT)
     } catch (error) {
