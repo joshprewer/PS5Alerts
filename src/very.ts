f43c8831d55285d47d32d4e29befd999afba646b
Josh Prewer <josh.prewer@focusrite.com>
2020-12-27 13:20:59 +0000

    Simplify very headers

--- src/very.ts
+++ src/very.ts
@@ -9,17 +9,15 @@ export async function veryHandler (): Promise<CheckProductResponse> {
     {
       headers: {
         'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
-        Accept: 'text/html',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+        Accept: 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-Language': 'en-GB,en;q=0.5',
-        Referer: 'https://www.google.com/',
         Pragma: 'no-cache',
         'Cache-Control': 'no-cache',
         'Accept-Encoding': 'gzip, deflate, br',
         Cookie: null
       }
-    }
-  )
+    })
 
   const redirectURL = response.url === 'https://www.very.co.uk/'
   const isAvailable = !response.redirected || !redirectURL
